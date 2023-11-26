8165c2b78524ad426c9eb1a15e273a88991c7ce0
jahidRahmanBBPI <144985079+jahidRahmanBBPI@users.noreply.github.com>
2023-11-26 23:01:19 +0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('component.home')
+    @include('component.about')
+    @include('component.project')
+    @include('component.contact')
+
+@endsection
